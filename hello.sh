82ee71b0931752680eacd21b83856f33b129e3ee
rakshi6361 <ksrakshith621@gmail.com>
2026-04-14 22:00:14 +0530

    Fixed merge conflict in hello.sh

--- hello.sh
+++ hello.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+
 echo "================================"
 echo "App Version: 1.0"
 echo "Build by Jenkins"
 echo "Date: $(date)"
 echo "================================"
-<<<<<<< HEAD
-=======
-echo "New line added!"
->>>>>>> 3faf30308ba92edc53d242f690884fa403309fe0
